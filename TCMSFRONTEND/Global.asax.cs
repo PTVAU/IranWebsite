3af922f97e7259bd8dbb9d16061543bba32ea70f
MJ.Pournassari <Mjpournassari@gmail.com>
2015-12-03 15:24:35 +0330

    * cross origin

--- TCMSFRONTEND/Global.asax.cs
+++ TCMSFRONTEND/Global.asax.cs
@@ -53,6 +53,14 @@ namespace TCMSFRONTEND
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
+            if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
+            {
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, TRACE, OPTIONS, HEAD");
+                HttpContext.Current.Response.AddHeader("Access-Control-Allow-Headers", "Authorization, Origin, Content-Type, Accept, X-Requested-With, accept, cache-control, x-requested-with, content-type");
+                HttpContext.Current.Response.AddHeader("Access-Control-Max-Age", "1728000");
+                HttpContext.Current.Response.End();
+            }
             RegisterCustomRoutes(RouteTable.Routes);
         }
 
